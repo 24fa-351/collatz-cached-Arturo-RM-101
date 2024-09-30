d57a29580766ae57f455b93318dd2cef43a08f3b
Arturo-RM-101 <111911678+Arturo-RM-101@users.noreply.github.com>
2024-09-30 00:42:31 -0700

    Uploaded Files of Attempt (Completed bullets 1-3)

---
+++ cache.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cache.h"
+
+
+// Works on allocating our dynamic memory using our typedefine
+// Inserted at the h file
+Cache *Cache_Entry(int Max_Capacity) {
+    Cache *Cache_Storage = (Cache*)malloc(sizeof(Cache));
+} 
+
+
+int Cache_Storage() {
+}
+
+int Hit_Rate() {
+}
+
+int Get_Steps() {
+}
---
+++ collatz.h
@@ -0,0 +1,8 @@
+#ifndef COLLATZ_H
+#define COLLATZ_H
+
+// This is our function definition that was utlilized by collatz.c
+// Which is defined and calls the corresponding function
+int Collatz_Conjecture(int Random_Number);
+
+#endif
