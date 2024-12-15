434be27e7a10b95f4d000ee3f9df1d945b71687d
Arturo-RM-101 <111911678+Arturo-RM-101@users.noreply.github.com>
2024-12-14 17:01:36 -0800

    Reworked on collatz.c

--- collatz.c
+++ collatz.c
@@ -3,54 +3,69 @@
 #include <time.h>
 
 #include "cache.h"
-#include "collatz.h"
 
-// Runs when called through the h file
-int Collatz_Conjecture(int Random_Number) {
-    // Initializes Counter to prevent adding counts
-    // together rather than individually
-    int Step_Counter = 0;
-    // Loops while the Random_Number isn't 1
-    while (Random_Number != 1) {
-            // If it's even we proceed with dividing by 2
-            if ((Random_Number % 2) == 0) {
-                Random_Number = Random_Number / 2;
-                Step_Counter += 1;
-            // If it's odd we proceed with multiplying by 3 and adding 1
-            } else if ((Random_Number % 2) == 1) {
-                Random_Number = (Random_Number * 3) + 1;
-                Step_Counter += 1;
-            }
-        }
-    // Return our counter once the Random_Number is 1
-    return Step_Counter;
+bool is_even(int number) {
+    return (number % 2) == 0;
 }
 
-// Our line arguments are inserted onto our main
-// Stores each of the corresponding arguments to their respective variables.
-int main(int argc, char* argv[]) {
-    int Num_Of_Tests = atoi(argv[1]);
-    int Smallest_Value = atoi(argv[2]);
-    int Largest_Value = atoi(argv[3]);
-    //char *policy = argv[4]; 
-    //int Cache_Size = atoi(argv[5]);
+// Returns the number of steps it takes to reach 1
+// test_number has to be greater or equal to 1
+unsigned int collatz_conjecture(unsigned int test_number) {
+    unsigned int original_test_number = test_number;
 
-    // Runs the Caclulations of collatz conjecture with the given Num_of_Tests
-    for (int ix = 0; ix < Num_Of_Tests; ix++) {
-        // This gives our resulting random number
-        // Within the range from Smallest_Value and Largest_Value
-        int Random_Number = rand() % 
-                (Largest_Value - Smallest_Value + 1) + Smallest_Value;
-
-        // Prints our number received from Random_Number
-        printf("Random Number: %d\n", Random_Number);
-
-        // Run over Collatz_Conjecture calling it from h and using our value
-        int Step_Counter = Collatz_Conjecture(Random_Number);
-
-        // Once Calculated returns to print out the Total Steps it took
-        printf("Total Steps: %d\n", Step_Counter);
+    if (cache_has(test_number)) {
+        return cache_get(test_number);
     }
+
+    unsigned int step_counter = 0;
+    while (test_number != 1) {
+        if (is_even(test_number)) {
+            test_number = test_number / 2;
+        } else {
+            test_number = (test_number * 3) + 1;
+        }
+        step_counter += 1;
+    }
+
+    cache_put(original_test_number, step_counter);
+
+    return step_counter;
+}
+
+int random_number_between(int smallest, int biggest) {
+    return rand() % (biggest - smallest + 1) + smallest;
+}
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 4) {
+        printf("Usage: %s <Num_of_Tests> <Smallest_Value> <Largest_Value>\n", argv[0]);
+        return 1;
+    }
+
+    unsigned int number_of_tests = atoi(argv[1]);
+    unsigned int smallest_value = atoi(argv[2]);
+    unsigned int largest_value = atoi(argv[3]);
+
+    for (int ix = 0; ix < number_of_tests; ix++) {
+        unsigned int random_number = random_number_between(smallest_value, largest_value);
+
+        unsigned int step_counter = collatz_conjecture(random_number);
+
+        printf("%u Steps, %u Number\n", step_counter, random_number);
+    }
+
+    unsigned int cache_hits = actual_cache_hits();
+    unsigned int cache_tests = actual_cache_tests();
+    
+    // Prevent this to divide by zero when calculating hit rate
+    if (cache_tests == 0) {
+        cache_tests = 1;
+    }
+
+    double hit_rate = (double)cache_hits / cache_tests;
+
+    printf("The hit rate over number of %u tests is: %f\n", cache_tests, hit_rate);
 
     return 0;
 }
