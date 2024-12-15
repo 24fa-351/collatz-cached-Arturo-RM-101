3f8c0fa3dff3b6c8bf6df1a2ac2d3642b48ea624
Arturo-RM-101 <111911678+Arturo-RM-101@users.noreply.github.com>
2024-12-14 17:02:33 -0800

    Collatz.h is no longer needed and function was sent onto collatz.c

--- collatz.h
+++
@@ -1,8 +0,0 @@
-#ifndef COLLATZ_H
-#define COLLATZ_H
-
-// This is our function definition that was utlilized by collatz.c
-// Which is defined and calls the corresponding function
-int Collatz_Conjecture(int Random_Number);
-
-#endif
